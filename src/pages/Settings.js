be0475f1abd5f9a7c552d3fd2e49e8cffb866688
doobmarley <47475664+doobmarley@users.noreply.github.com>
2026-05-05 18:04:01 +0200

    Add Settings component with initial layout

---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Settings.css';
+
+function Settings() {
+  return (
+    <div className="settings">
+      <h2>Configuració</h2>
+      <p className="info">Aquesta secció s'anirà completant properament.</p>
+    </div>
+  );
+}
+
+export default Settings;
