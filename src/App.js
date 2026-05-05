1d4df2bf5b2a6045eb168f998255a963263f8873
doobmarley <47475664+doobmarley@users.noreply.github.com>
2026-05-05 18:26:21 +0200

    Fetch installed modules and pass to Store component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Modules from './pages/Modules';
+import Store from './pages/Store';
 import Settings from './pages/Settings';
 import Navbar from './components/Navbar';
 import './App.css';
 
+const AGENT_URL = 'http://192.168.1.97:8585';
+
 function App() {
+  const [installedModules, setInstalledModules] = useState([]);
+
+  useEffect(() => {
+    fetch(`${AGENT_URL}/modules`)
+      .then(res => res.json())
+      .then(data => setInstalledModules(data.modules || []));
+  }, []);
+
   return (
     <Router>
       <div className="app">
@@ -15,6 +26,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/modules" element={<Modules />} />
+            <Route path="/store" element={<Store installedModules={installedModules} />} />
             <Route path="/settings" element={<Settings />} />
           </Routes>
         </main>
